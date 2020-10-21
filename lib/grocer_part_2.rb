85c13e64d1e96c2b8ace8fb709fcd172d4e10458
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-21 19:45:11 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -27,6 +27,7 @@ def apply_coupons(cart, coupons)
           added_item[:price] = coupon_item[:cost] / coupon_item[:num]
           added_item[:clearance] = grocery_item[:clearance]
           added_item[:count] = coupon_item[:num]
+          grocery_item[:count] = grocery_item[:count] - coupon_item[:num]
           binding.pry
         end
         # if grocery_item[:count] % coupon_item[:num] == 0 
