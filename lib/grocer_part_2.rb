a10f486c6ef07448848497c0dc7b9ee6182853bd
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-21 19:48:41 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -37,9 +37,7 @@ def apply_coupons(cart, coupons)
 end 
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+ 
 end
 
 def checkout(cart, coupons)
@@ -120,4 +118,9 @@ PART OF SECOND ATTEMPT:
         #   coupon_item[:count] = grocery_item[:count]
         #   cart.push(coupon_item)
         # end
+        
+APPLY CLEARANCE NOTES:
+# Consult README for inputs and outputs
+#
+# REMEMBER: This method **should** update cart
 =end
