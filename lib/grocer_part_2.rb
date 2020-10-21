33ed1f14d2b2663af31ca1de984db68552cd0589
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-21 20:01:44 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -45,6 +45,7 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
+  binding.pry
   consolidated_cart = consolidate_cart(cart)
   coupon_cart = apply_coupons(consolidated_cart)
   final_cart = apply_clearance(coupon_cart)
